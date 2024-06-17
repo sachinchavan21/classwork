42c8f924a325fe772ba7ceb90ef01c47bddf37a7
sachinchavan21597@gmail.com <sachinchavan21597@gmail.com>
2024-06-17 09:38:22 +0530

    my first push to mondays push

---
+++ src/practiceclass/stringContainsOvel2.java
@@ -0,0 +1,21 @@
+package practiceclass;
+
+public class stringContainsOvel2 {
+
+
+    public static void main(String[] args) {
+        String str = "Sachin";
+String str1 = str.toLowerCase();
+        char [] arr = str1.toCharArray();
+        for (int i=0;i<str.length();i++){
+            if (arr[i]=='a'|| arr[i]=='e'|| arr[i]=='i'|| arr[i]=='o'|| arr[i]=='u'){
+                System.out.println("String contains Vowel which is: "+arr[i]);
+                break;}
+//            }else {
+//                System.out.println("String contains Consanant which is: "+arr[i]);
+//            }
+        }
+
+
+    }}
+
